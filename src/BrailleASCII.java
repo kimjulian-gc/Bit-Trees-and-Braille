4f6003db4806f69ee779a4e77a3fa6ffd755d90d
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-16 21:24:10 -0600

    Finished BrailleASCII

---
+++ src/BrailleASCII.java
@@ -0,0 +1,72 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import resources.BrailleASCIITables;
+
+public class BrailleASCII {
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      throw new IllegalArgumentException("Invalid number of arguments: got " + args.length + ", needs 2");
+    }
+
+    Converter converter;
+    switch (args[0].toLowerCase()) {
+      case "braille":
+        converter = BrailleASCII::convertToBraille;
+        break;
+      case "ascii":
+        converter = BrailleASCII::convertToASCII;
+        break;
+      case "unicode":
+        converter = BrailleASCII::convertToUnicode;
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid target character set: got " + args[0]);
+    }
+    
+    PrintWriter pen = new PrintWriter(System.out, true);
+    pen.println(converter.convert(args[1]));
+
+  }
+
+  public static String convertToBraille(String str) throws IOException {
+    String output = "";
+    for (char c : str.toCharArray()) {
+      output += BrailleASCIITables.toBraille(c);
+    }
+    return output;
+  }
+
+  public static String convertToASCII(String bits) throws IOException {
+    boolean isString = bits.length() > 6;
+    if (!isString) return BrailleASCIITables.toASCII(bits);
+
+    // bits is a string of braille
+    String output = "";
+    for (int i = 0; i < bits.length(); i += 6) {
+      String subBits;
+      if (i + 6 <= bits.length()) subBits = bits.substring(i, i + 6);
+      else subBits = bits.substring(i);
+      output += BrailleASCIITables.toASCII(subBits);
+    }
+    return output;
+  }
+
+  public static String convertToUnicode(String str) throws IOException {
+    String bits = convertToBraille(str);
+    
+    String output = "";
+    for (int i = 0; i < bits.length(); i += 6) {
+      String subBits;
+      if (i + 6 <= bits.length()) subBits = bits.substring(i, i + 6);
+      else subBits = bits.substring(i);
+      output += BrailleASCIITables.toUnicode(subBits);
+    }
+    return output;
+  }
+
+  @FunctionalInterface
+  public interface Converter {
+    String convert(String str) throws IOException;
+  }
+
+}
--- src/resources/BrailleASCIITables.java
+++ src/resources/BrailleASCIITables.java
@@ -1,3 +1,5 @@
+package resources;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,20 +14,21 @@ public class BrailleASCIITables {
    * converts an ASCII character to a string of bits representing the corresponding braille
    * character.
    */
-  static String toBraille(char letter) throws IOException {
+  public static String toBraille(char letter) throws IOException {
     if (BrailleASCIITables.asciiToBrailleTree == null) {
       BrailleASCIITables.asciiToBrailleTree = new BitTree(8);
       asciiToBrailleTree.load(new FileInputStream(new File("src/resources/ASCIIToBraille.csv")));
     }
-    
-    return asciiToBrailleTree.get(Integer.toBinaryString(letter));
+
+    return asciiToBrailleTree
+        .get(String.format("%8s", Integer.toBinaryString(letter)).replace(' ', '0'));
   }
 
   /**
    * converts a string of bits representing a braille character to the corresponding ASCII
    * character.
    */
-  static String toASCII(String bits) throws IOException {
+  public static String toASCII(String bits) throws IOException {
     if (BrailleASCIITables.brailleToAsciiTree == null) {
       BrailleASCIITables.brailleToAsciiTree = new BitTree(6);
       brailleToAsciiTree.load(new FileInputStream(new File("src/resources/BrailleToASCII.csv")));
@@ -38,12 +41,13 @@ public class BrailleASCIITables {
    * converts a string of bits representing a braille character to the corresponding Unicode braille
    * character for those bits. Only supports six-bit braille characters.
    */
-  static String toUnicode(String bits) throws IOException {
+  public static char toUnicode(String bits) throws IOException {
     if (BrailleASCIITables.brailleToUnicodeTree == null) {
       BrailleASCIITables.brailleToUnicodeTree = new BitTree(6);
-      brailleToUnicodeTree.load(new FileInputStream(new File("src/resources/BrailleToUnicode.csv")));
+      brailleToUnicodeTree
+          .load(new FileInputStream(new File("src/resources/BrailleToUnicode.csv")));
     }
 
-    return brailleToUnicodeTree.get(bits);
+    return Character.toChars(Integer.parseInt(brailleToUnicodeTree.get(bits), 16))[0];
   }
 }

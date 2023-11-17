0ccedeaba43bca1d5e15033b97b984a4978079c7
kerabite <10791585+kerabite@users.noreply.github.com>
2023-11-16 21:25:34 -0600

    Formatted BitTree and BrailleASCII

--- src/BrailleASCII.java
+++ src/BrailleASCII.java
@@ -5,7 +5,8 @@ import resources.BrailleASCIITables;
 public class BrailleASCII {
   public static void main(String[] args) throws IOException {
     if (args.length != 2) {
-      throw new IllegalArgumentException("Invalid number of arguments: got " + args.length + ", needs 2");
+      throw new IllegalArgumentException(
+          "Invalid number of arguments: got " + args.length + ", needs 2");
     }
 
     Converter converter;
@@ -22,7 +23,7 @@ public class BrailleASCII {
       default:
         throw new IllegalArgumentException("Invalid target character set: got " + args[0]);
     }
-    
+
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println(converter.convert(args[1]));
 
@@ -38,14 +39,17 @@ public class BrailleASCII {
 
   public static String convertToASCII(String bits) throws IOException {
     boolean isString = bits.length() > 6;
-    if (!isString) return BrailleASCIITables.toASCII(bits);
+    if (!isString)
+      return BrailleASCIITables.toASCII(bits);
 
     // bits is a string of braille
     String output = "";
     for (int i = 0; i < bits.length(); i += 6) {
       String subBits;
-      if (i + 6 <= bits.length()) subBits = bits.substring(i, i + 6);
-      else subBits = bits.substring(i);
+      if (i + 6 <= bits.length())
+        subBits = bits.substring(i, i + 6);
+      else
+        subBits = bits.substring(i);
       output += BrailleASCIITables.toASCII(subBits);
     }
     return output;
@@ -53,12 +57,14 @@ public class BrailleASCII {
 
   public static String convertToUnicode(String str) throws IOException {
     String bits = convertToBraille(str);
-    
+
     String output = "";
     for (int i = 0; i < bits.length(); i += 6) {
       String subBits;
-      if (i + 6 <= bits.length()) subBits = bits.substring(i, i + 6);
-      else subBits = bits.substring(i);
+      if (i + 6 <= bits.length())
+        subBits = bits.substring(i, i + 6);
+      else
+        subBits = bits.substring(i);
       output += BrailleASCIITables.toUnicode(subBits);
     }
     return output;
